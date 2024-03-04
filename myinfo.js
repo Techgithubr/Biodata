17b325cd1088af4f4059ee6f71c2b676d839f80e
Saif Iqbal Ghouri <160517769+Techgithubr@users.noreply.github.com>
2024-03-03 23:15:49 -0800

    first commit

---
+++ myinfo.js
@@ -0,0 +1,61 @@
+"use strict";
+// Persnol information:
+let fullName = "SAIF IQBAL GHOURI";
+let fatherName = "IQBAL RASHEED GHOURI";
+let age = 18;
+let isGender = "Male";
+let isStudent = true;
+let isEmployed = false;
+let isMarried = false;
+const contact = "0319-2482834";
+const email = "saifiqbal371@gmail.com";
+const religion = "Islam";
+// Adress:
+let address = "House A/58 dalmia shanti nagar karachi";
+let area = "Dalmia";
+let city = "Karachi";
+let country = "Pakistan";
+// skills:
+const skills = "Ms Office,Graphic Desiging,Artificial Intellegence(AI),Metaverse & Web 3.0 Devoloper (In Process)";
+// Language:
+const lang = "English,Urdu";
+// Education:
+const schoolName = "M.D Secondary School";
+const degree = "Matric";
+const year = "2022";
+const collegename = "Govt.Degree Johar College Boys (Morning)";
+const Degree = "In Progress";
+const Year = "2024";
+// Experience:
+let experience = "Fresh";
+//Printing Biodata:
+console.log("PERSNOL INFORMATION:-");
+console.log("Name:", fullName);
+console.log("Father Name:", fatherName);
+console.log("Age:", age);
+console.log("Gender:", isGender);
+console.log("Student:", isStudent);
+console.log("Employed:", isEmployed);
+console.log("Married:", isMarried);
+console.log("Contact:", contact);
+console.log("E-Mail:", email);
+console.log("Religion:", religion);
+console.log("ADDRESS:-");
+console.log("Area:", area);
+console.log("City:", city);
+console.log("Country:", country);
+console.log("Adress:", address);
+console.log("SKILLS:-");
+console.log("Skill:", skills);
+console.log("LANGUAGES:-");
+console.log("Language:", lang);
+console.log("MATRICULATION:-");
+console.log("School:", schoolName);
+console.log("Degree:", degree);
+console.log("Year:", year);
+console.log("INTERMEDIATE:-");
+console.log("College:", collegename);
+console.log("Degree:", Degree);
+console.log("Year:", Year);
+console.log("EXPERIENCE:-");
+console.log(experience);
---
+++ myinfo.ts
@@ -0,0 +1,69 @@
+// Persnol information:
+let fullName: string = "SAIF IQBAL GHOURI"
+let fatherName: string = "IQBAL RASHEED GHOURI"
+let age: number = 18
+let isGender: string = "Male"
+let isStudent: boolean = true
+let isEmployed: boolean = false
+let isMarried: boolean = false 
+const contact: string = "0319-2482834"
+const email: string = "saifiqbal371@gmail.com"
+const religion: string = "Islam"
+
+// Adress:
+let address: string = "House A/58 dalmia shanti nagar karachi"
+let area: string ="Dalmia"
+let city: string = "Karachi"
+let country: string = "Pakistan"
+
+// skills:
+const skills: string = "Ms Office,Graphic Desiging, Artificial Intellegence(AI),Metaverse & Web 3.0 Devoloper (In Process)"
+
+// Language:
+const lang: string ="English,Urdu"
+
+// Education:
+const schoolName: string = "M.D Secondary School"
+const degree: string = "Matric"
+const year: string = "2022"
+const collegename: string = "Govt.Degree Johar College Boys (Morning)"
+const Degree: string = "In Progress"
+const Year: string = "2024"
+
+// Experience:
+let experience: string = "Fresh"
+
+//Printing Biodata:
+console.log("PERSNOL INFORMATION:-")
+console.log("Name:",fullName)
+console.log("Father Name:",fatherName)
+console.log("Age:",age)
+console.log("Gender:",isGender)
+console.log("Student:",isStudent)
+console.log("Employed:",isEmployed)
+console.log("Married:",isMarried)
+console.log("Contact:",contact)
+console.log("E-Mail:",email)
+console.log("Religion:",religion)
+console.log("ADDRESS:-")
+console.log("Area:",area)
+console.log("City:",city)
+console.log("Country:",country)
+console.log("Adress:",address)
+console.log("SKILLS:-")
+console.log("Skill:",skills)
+console.log("LANGUAGES:-")
+console.log("Language:",lang)
+console.log("MATRICULATION:-")
+console.log("School:",schoolName)
+console.log("Degree:",degree)
+console.log("Year:",year)
+console.log("INTERMEDIATE:-")
+console.log("College:",collegename)
+console.log("Degree:",Degree)
+console.log("Year:",Year)
+console.log("EXPERIENCE:-")
+console.log(experience)
+
+
+
